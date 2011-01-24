126fa573a37073b2303f3b128919a78f94b1de4e
puzanov <puzanov@gmail.com>
2011-01-24 10:58:52 +0600

    added config file for twitter oauth variables

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,4 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Twilist::Application.initialize!
+TWITTER_CONFIG = YAML.load_file("#{RAILS_ROOT}/config/twitter.yml")
---
+++ lib/twitter_fetcher.rb
@@ -0,0 +1,25 @@
+class TwitterFetcher
+  attr_accessor :client
+
+  def initialize
+    Twitter.configure do |config|
+      config.consumer_key = TWITTER_CONFIG["consumer_key"]
+      config.consumer_secret = TWITTER_CONFIG["consumer_secret"]
+      config.oauth_token = TWITTER_CONFIG["oauth_token"]
+      config.oauth_token_secret = TWITTER_CONFIG["oauth_token_secret"]
+    end
+    @client = Twitter::Client.new
+  end
+
+  def update_friends_timeline
+    puts @client.friends_timeline.first.to_yaml
+    @client.friends_timeline(:count => 200).each do |friend| 
+      tweet = Tweet.new
+      tweet.twitter_id = friend.id_str
+      tweet.username = friend.user.name
+      tweet.text = friend.text
+      tweet.profile_image_url = friend.user.profile_image_url
+      tweet.save
+    end
+  end
+end
